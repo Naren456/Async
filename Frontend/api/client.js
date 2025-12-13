54d71fb6a41027b8974297f10276b6fb1a700ce1
Naren456 <narendrase666@gmail.com>
2025-12-13 11:11:44 +0530

    fix security bugs

--- Frontend/api/client.js
+++ Frontend/api/client.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
-
-const Production = "https://async-vr1z.onrender.com";
-const Local = "http://10.51.6.234:8000";
-
-export const API_BASE_URL = Production;
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 const client = axios.create({
   baseURL: API_BASE_URL,
