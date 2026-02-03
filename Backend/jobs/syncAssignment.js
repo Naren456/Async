ed34125f915bfc0d39612acd62b213eef5168c82
Naren456 <narendrase666@gmail.com>
2026-02-03 20:49:23 +0530

    update backend

--- Backend/jobs/syncAssignment.js
+++ Backend/jobs/syncAssignment.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
 import { main as syncAssignments } from "../utils/SyncCoursera.js";
 
-// Schedule to run daily at 12:00 AM
-cron.schedule("0 * * * *", async () => {
-  console.log("🌙 Running daily Coursera sync...");
+// Schedule to run every hour at minute 0
+cron.schedule("* * * * *", async () => {
+  console.log("⏰ Running hourly Coursera sync...");
   await syncAssignments();
 });
